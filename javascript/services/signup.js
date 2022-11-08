f900188553d119fad3d3c52545fa7ab11891bc18
4ouR04 <amosmwongelah@gmail.com>
2022-11-08 20:57:09 +0300

    Welcome user

--- javascript/services/signup.js
+++ javascript/services/signup.js
@@ -2,5 +2,7 @@ const mysql = require('myql2')
 const config = require('../config/config')
 const db = mysql.createConnection(config)
 const welcomeUser = async()=>{
+    const query = `SELECT * FROM users WHERE isWelcome = false`
+    db.query(query,()=>{})
     
 }
