539c1c36195c0372fcb15eec9b0694a5e6958c77
4ouR04 <amosmwongelah@gmail.com>
2022-11-11 23:02:13 +0300

    Update controller

--- javascript/services/signup.js
+++ javascript/services/signup.js
@@ -3,6 +3,6 @@ const config = require('../config/config')
 const db = mysql.createConnection(config)
 const welcomeUser = async()=>{
     const query = `SELECT * FROM users WHERE isWelcome = false`
-    db.query(query,()=>{})
+    db.query(query,(error, users)=>{})
     
 }
